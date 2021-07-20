a678452a0805c607241cab5548534efc8bf296e3
Stas Voronin <jameslebron@yandex.ru>
2021-07-20 23:15:11 +0300

    feat: сверстал компонент Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Main.css';
+import Promo from '../Promo/Promo';
+import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 function Main() {
   return (
-    <h2>Main</h2>
+    <main className="main">
+      <Promo />
+      <NavTab />
+      <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
+    </main>
   );
 }
 
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Promo.css';
+import Image from '../../images/cover.svg';
 
 function Promo() {
   return (
-    <h2>Promo</h2>
+    <section className="promo">
+      <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+      <img className="promo__image" src={Image} alt="Буква П в геометрической сетке" />
+    </section>
   );
 }
 
