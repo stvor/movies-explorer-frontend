db902eb536a137fafde5bf301d095c36c0dd1420
Stas Voronin <jameslebron@yandex.ru>
2021-07-20 21:52:16 +0300

    feat: добавил ссылки в шапку для проверки роутинга

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <h2>Header</h2>
+    <header className="header">
+      <h2>Header</h2>
+      <Link to="/"> О проекте </Link>
+      <Link to="/movies"> Фильмы </Link>
+      <Link to="/saved-movies"> Сохранённые фильмы </Link>
+      <Link to="/signup"> Регистрация </Link>
+      <Link to="/signin"> Авторизация </Link>
+      <Link to="/profile"> Аккаунт </Link>
+    </header>
   );
 }
 
