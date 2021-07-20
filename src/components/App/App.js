71ff4afcdba835fe86261066183b29100593117d
Stas Voronin <jameslebron@yandex.ru>
2021-07-20 21:33:17 +0300

    feat: добавил роутинг и основные роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import PageNotFound from '../PageNotFound/PageNotFound';
 import './App.css';
 
 function App() {
   return (
-    <h2>App</h2>
+    <div className="page">
+      <div className="page__container">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+        <Footer />
+
+      </div>
+    </div>
   );
 }
 
