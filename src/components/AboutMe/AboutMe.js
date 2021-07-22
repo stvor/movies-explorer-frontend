46675271fa7403cc2ba9aa7404ac8915e18c5abe
Stas Voronin <jameslebron@yandex.ru>
2021-07-22 16:47:59 +0300

    feat: сверстал компонент AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './AboutMe.css';
+import Photo from '../../images/photo.jpg';
 
 function AboutMe() {
   return (
-    <h2>AboutMe</h2>
+    <section className="about-me">
+      <h2 className="about-me__title" id="student">Студент</h2>
+      <div className="about-me__wrap">
+        <div className="about-me__info">
+          <h3 className="about-me__name">Станислав</h3>
+          <p className="about-me__profile">Фронтенд-разработчик, 32 года</p>
+          <p className="about-me__description">Я&nbsp;родился и&nbsp;живу в&nbsp;Москве, с&nbsp;отличием закончил факультет налогов и&nbsp;налогового менеджмента в&nbsp;ГУУ. В&nbsp;прошлом я&nbsp;менеджер&nbsp;IT проектов (5&nbsp;лет опыта, из&nbsp;них 3&nbsp;года в&nbsp;Яндексе). Сейчас я&nbsp;работаю тестировщиком на&nbsp;проекте МТС Библиотека и&nbsp;заканчиваю обучение в&nbsp;Яндекс.Практикуме по&nbsp;специальности &laquo;Веб-разработчик&raquo;. Понимаю, что хочу создавать ценность своими руками, поэтому ищу позицию фронтенд разработчика.</p>
+          <ul className="about-me__links-list">
+            <li>
+              <a className="about-me__links-item" href="https://www.facebook.com/manhat10/" target="_blank" rel="noreferrer">Facebook</a>
+            </li>
+            <li>
+            <a className="about-me__links-item" href="https://github.com/stvor" target="_blank" rel="noreferrer">Github</a>
+            </li>
+          </ul>
+        </div>
+        <img className="about-me__photo" src={Photo} alt="Фотография студента" />
+      </div>
+    </section>
   );
 }
 
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Promo.css';
-import Image from '../../images/cover.svg';
+import Cover from '../../images/cover.svg';
 
 function Promo() {
   return (
     <section className="promo">
       <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
-      <img className="promo__image" src={Image} alt="Буква П в геометрической сетке" />
+      <img className="promo__image" src={Cover} alt="Буква П в геометрической сетке" />
     </section>
   );
 }
