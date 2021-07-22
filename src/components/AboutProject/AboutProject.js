0d58f512476238558177b26217e3913b805f285b
Stas Voronin <jameslebron@yandex.ru>
2021-07-22 11:43:39 +0300

    feat: сверстал компонент AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,7 +3,30 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <h2>AboutProject</h2>
+    <section className="about-project">
+      <h2 className="about-project__title" id="about-project">О проекте</h2>
+      <div className="about-project__features">
+        <article className="about-project__feature">
+          <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </article>
+        <article className="about-project__feature">
+          <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </article>
+      </div>
+      <div className="about-project__timeline timeline">
+        <div className="timeline__bar-wrap">
+          <div className="timeline__bar timeline__bar_backend">1 неделя</div>
+          <div className="timeline__bar timeline__bar_frontend">4 недели</div>
+        </div>
+        <div className="timeline__caption-wrap">
+          <div className="timeline__caption timeline__caption_backend">Back-end</div>
+          <div className="timeline__caption timeline__caption_frontend">Front-end</div>
+        </div>
+      </div>
+    </section>
+
   );
 }
 
