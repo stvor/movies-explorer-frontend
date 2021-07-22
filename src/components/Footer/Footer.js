f4f2200bdb33aafecc72326e1453016343cdb35a
Stas Voronin <jameslebron@yandex.ru>
2021-07-22 18:05:47 +0300

    feat: сверстал компонент Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,23 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <h2>Footer</h2>
+    <section className="footer">
+      <p className="footer__caption">Учебный проект Яндекс.Практикум × BeatFilm.</p>
+      <div className="footer__wrap">
+        <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+        <ul className="footer__links-list">
+          <li>
+            <a className="footer__links-item" href="https://praktikum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+          </li>
+          <li>
+            <a className="footer__links-item" href="https://github.com/stvor" target="_blank" rel="noreferrer">Github</a>
+          </li>
+          <li>
+            <a className="footer__links-item" href="https://www.facebook.com/manhat10/" target="_blank" rel="noreferrer">Facebook</a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
