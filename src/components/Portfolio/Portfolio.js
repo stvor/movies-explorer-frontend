aec221c0e361d5bb98378a2a4ee2a8a434ad4eb0
Stas Voronin <jameslebron@yandex.ru>
2021-07-22 17:21:25 +0300

    feat: сверстал компонент Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Portfolio.css';
+import LinkIcon from '../../images/link-icon.svg';
 
 function Portfolio() {
   return (
-    <h2>Portfolio</h2>
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__links-list">
+        <li className="portfolio__links-item">
+          <a className="portfolio__link" href="https://github.com/stvor/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт</a>
+          <img className="portfolio__link-icon" src={LinkIcon} alt="Иконка для ссылки" />
+        </li>
+        <li className="portfolio__links-item">
+          <a className="portfolio__link" href="https://github.com/stvor/russian-travel" target="_blank" rel="noreferrer">Адаптивный сайт</a>
+          <img className="portfolio__link-icon" src={LinkIcon} alt="Иконка для ссылки" />
+        </li>
+        <li className="portfolio__links-item">
+          <a className="portfolio__link" href="https://github.com/stvor/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение</a>
+          <img className="portfolio__link-icon" src={LinkIcon} alt="Иконка для ссылки" />
+        </li>
+      </ul>
+    </section>
   );
 }
 
