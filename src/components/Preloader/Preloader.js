f6a6747fbd4308eb5a2d59aa782b4a92e4b704b9
Stas Voronin <jameslebron@yandex.ru>
2021-07-24 19:43:26 +0300

    feat: добавил код компонента Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -3,8 +3,12 @@ import './Preloader.css';
 
 function Preloader() {
   return (
-    <h2>Preloader</h2>
-  );
-}
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  )
+};
 
 export default Preloader;
