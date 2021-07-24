8713d9d1e9f1ff0611a83b138053afad9b6c61d0
Stas Voronin <jameslebron@yandex.ru>
2021-07-24 19:34:04 +0300

    feat: сверстал компонент Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
+import Logo from '../../images/logo.svg';
 
 function Login() {
   return (
-    <h2>Login</h2>
+    <section className="login">
+      <form className="form" name="login" action="#" noValidate>
+        <img className="form__logo" src={Logo} alt="Логотип сайта" />
+        <h1 className="form__title">Рады видеть!</h1>
+
+        <label className="form__label" for="email-input">E-mail</label>
+        <input id="email-input" type="text" name="email" placeholder="E-mail" className="form__input form__input_type_email" />
+        <span className="email-input-error form__input-error">Что-то пошло не так...</span>
+
+        <label className="form__label" for="password-input">Пароль</label>
+        <input id="password-input" type="password" name="password" placeholder="Пароль" className="form__input form__input_type_password" />
+        <span className="password-input-error form__input-error">Что-то пошло не так...</span>
+
+        <button type="submit" className="form__submit">Войти</button>
+
+        <div className="form__sign-in-wrap">
+          <p className="form__sign-in-question">Ещё не зарегистрированы? <Link className="form__sign-in-link" to="/signup">Регистрация</Link></p>
+        </div>
+      </form>
+    </section>
   );
 }
 
