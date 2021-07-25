123bc49e54f1eacf904f0d03be0ce1bd53aa2309
Stas Voronin <jameslebron@yandex.ru>
2021-07-26 00:07:16 +0300

    feat: сверстал компонент SavedMovies

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MoviesCard.css';
 import CardCover from '../../images/card-cover.png';
+import { Route, Switch } from 'react-router-dom';
 
 function MoviesCard() {
   return (
@@ -10,7 +11,14 @@ function MoviesCard() {
           <h2 className="movies-card__title">33 слова о дизайне</h2>
           <span className="movies-card__duration">1ч 47м</span>
         </div>
-        <button className="movies-card__save" type="button"></button>
+        <Switch>
+          <Route path="/movies">
+            <button className="movies-card__button movies-card__button_type_save" type="button"></button>
+          </Route>
+          <Route path="/saved-movies">
+            <button className="movies-card__button movies-card__button_type_delete" type="button"></button>
+          </Route>
+        </Switch>
       </div>
       <img className="movies-card__cover" src={CardCover} alt="Обложка фильма"/>
     </li>
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
 import './SavedMovies.css';
 
 function SavedMovies() {
   return (
-    <h2>SavedMovies</h2>
+  <section className="saved-movies">
+    <SearchForm />
+    <MoviesCardList />
+  </section>
+
   );
 }
 
