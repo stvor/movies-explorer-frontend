e73ff6ce6aaa26873922f0f341024f0499c0ea6a
Stas Voronin <jameslebron@yandex.ru>
2021-07-25 12:22:04 +0300

    feat: сверстал компонент Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,22 @@ import './Profile.css';
 
 function Profile() {
   return (
-    <h2>Profile</h2>
+    <section className="profile">
+      <form className="profile-form" name="login" action="#" noValidate>
+        <h1 className="profile-form__title">Привет, Виталий!</h1>
+
+          <label className="profile-form__label" for="name-input">Имя
+            <input id="name-input" type="text" name="name" placeholder="Имя" className="profile-form__input profile-form__input_type_name" />
+          </label>
+
+        <label className="profile-form__label" for="email-input">E-mail
+          <input id="email-input" type="text" name="email" placeholder="E-mail" className="profile-form__input profile-form__input_type_email" />
+        </label>
+
+        <button type="submit" className="profile-form__submit">Редактировать</button>
+        <button className="profile__logout">Выйти из аккаунта</button>
+      </form>
+    </section>
   );
 }
 
