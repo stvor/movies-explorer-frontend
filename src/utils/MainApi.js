3fc038f20211e563aa464870560a1c26c8b02fa7
Stas Voronin <jameslebron@yandex.ru>
2021-08-02 21:11:04 +0300

    feat: добавил API для работы с пользователем

---
+++ src/utils/MainApi.js
@@ -0,0 +1,67 @@
+class MainApi {
+  constructor({ url }) {
+    this.url = url;
+  }
+
+  _processingResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(new Error(`Ошибка, код ${res.status}`))
+  }
+
+  signUp({ email, name, password }) {
+    return fetch(`${this.url}/signup`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, name, password }),
+    })
+      .then(res => this._processingResponse(res));
+  }
+
+  signIn({ email, password }) {
+    return fetch(`${this.url}/signin`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then(res => this._processingResponse(res));
+  }
+
+  getUser(jwt) {
+    return fetch(`${this.url}/users/me`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization" : `Bearer ${jwt}`,
+      },
+    })
+      .then(res => this._processingResponse(res));
+  }
+
+  updateUser({ userData, jwt}) {
+    return fetch(`${this.url}/users/me`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization" : `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({
+        name: userData.name,
+        email: userData.email
+      }),
+    })
+    .then(res => this._processingResponse(res));
+  }
+}
+
+const mainApi = new MainApi({
+  url: 'http://api.moviest.nomoredomains.monster',
+});
+
+export default mainApi;
