691768669bafc76483c024b501f359f62491a248
Stas Voronin <jameslebron@yandex.ru>
2021-09-02 23:04:22 +0300

    feat: добавил валидацию в форму регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,56 +2,98 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css';
 import Logo from '../../images/logo.svg';
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 
 function Register({ onRegister }) {
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  function handleNameChange(evt) {
-    setName(evt.target.value);
-  }
-
-  function handleEmailChange(evt) {
-    setEmail(evt.target.value);
-  }
-
-  function handlePasswordChange(evt) {
-    setPassword(evt.target.value);
-  }
+  const { values, handleChange, resetFrom, errors, isValid } = useFormWithValidation();
+  const submitButtonClassName = `form__submit ${
+    !isValid && "form__submit_inactive"
+  }`;
 
   function handleSubmit(evt) {
     evt.preventDefault();
-    onRegister({ name, email, password });
+    onRegister(values);
   }
+
+  React.useEffect(() => {
+    resetFrom({}, {}, false);
+  }, [resetFrom]);
 
   return (
     <section className="register">
-      <form onSubmit={handleSubmit} className="form" name="register" action="#" >
+      <form
+        onSubmit={handleSubmit}
+        className="form"
+        name="register"
+        action="#"
+      >
         <Link to="/">
-          <img className="form__logo" src={Logo} alt="Логотип сайта" />
+          <img
+            className="form__logo"
+            src={Logo}
+            alt="Логотип сайта"
+          />
         </Link>
         <h1 className="form__title">Добро пожаловать!</h1>
 
         <label className="form__label">
           <span className="form__label-text">Имя</span>
-          <input value={name || ''} onChange={handleNameChange} id="name-input" type="text" name="name" placeholder="Имя" className="form__input form__input_type_name" minLength="2" maxLength="30" required />
-          <span className="name-input-error form__input-error">Что-то пошло не так...</span>
+          <input
+            value={values.name || ''}
+            onChange={handleChange}
+            id="name-input"
+            type="text"
+            name="name"
+            placeholder="Имя"
+            className="form__input form__input_type_name"
+            minLength="2"
+            maxLength="30"
+            required
+          />
+          <span className="name-input-error form__input-error">
+            {errors.name || ''}
+          </span>
         </label>
 
         <label className="form__label">
           <span className="form__label-text">E-mail</span>
-          <input value={email || ''} onChange={handleEmailChange} id="email-input" type="text" name="email" placeholder="E-mail" className="form__input form__input_type_email" required />
-          <span className="email-input-error form__input-error">Что-то пошло не так...</span>
+          <input
+            value={values.email || ''}
+            onChange={handleChange}
+            id="email-input"
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            className="form__input form__input_type_email"
+            required
+          />
+          <span className="email-input-error form__input-error">
+            {errors.email || ''}
+          </span>
         </label>
 
         <label className="form__label" >
           <span className="form__label-text">Пароль</span>
-          <input value={password || ''} onChange={handlePasswordChange} id="password-input" type="password" name="password" placeholder="Пароль" className="form__input form__input_type_password" required />
-          <span className="password-input-error form__input-error">Что-то пошло не так...</span>
+          <input
+            value={values.password || ''}
+            onChange={handleChange}
+            id="password-input"
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            className="form__input form__input_type_password"
+            required
+          />
+          <span className="password-input-error form__input-error">
+            {errors.password || ''}
+          </span>
         </label>
 
-        <button type="submit" className="form__submit">Зарегистрироваться</button>
+        <button
+          type="submit"
+          className={submitButtonClassName}
+          disabled={!isValid}
+        >Зарегистрироваться</button>
 
         <div className="form__sign-in-wrap">
           <p className="form__sign-in-question">Уже зарегистрированы? <Link className="form__sign-in-link" to="/signin">Войти</Link></p>
