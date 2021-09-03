dc52627f56e84aa9231b51add284a99d3eb9ea6a
Stas Voronin <jameslebron@yandex.ru>
2021-09-03 21:51:11 +0300

    refactor: отформатировал код компонента Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -28,21 +28,52 @@ function Profile({ onProfileEdit, onSignOut }) {
 
   return (
     <section className="profile">
-      <form onSubmit={handleSubmit} className="profile-form" name="login" action="#" >
+      <form
+        onSubmit={handleSubmit}
+        className="profile-form"
+        name="login"
+        action="#"
+      >
         <h1 className="profile-form__title">Привет, {currentUser.name}!</h1>
 
         <label className="profile-form__label">
           <span className="profile-form__label-text">Имя</span>
-          <input value={name || ''} onChange={handleNameChange} id="name-input" type="text" name="name" placeholder="Имя" className="profile-form__input profile-form__input_type_name" minLength="2" maxLength="30" required />
+          <input
+            value={name || ''}
+            onChange={handleNameChange}
+            id="name-input"
+            type="text"
+            name="name"
+            placeholder="Имя"
+            className="profile-form__input profile-form__input_type_name"
+            minLength="2"
+            maxLength="30"
+            required
+          />
         </label>
 
         <label className="profile-form__label">
           <span className="profile-form__label-text">E-mail</span>
-          <input value={email || ''} onChange={handleEmailChange} id="email-input" type="text" name="email" placeholder="E-mail" className="profile-form__input profile-form__input_type_email" required />
+          <input
+            value={email || ''}
+            onChange={handleEmailChange}
+            id="email-input"
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            className="profile-form__input profile-form__input_type_email"
+            required
+          />
         </label>
 
-        <button type="submit" className="profile-form__submit">Редактировать</button>
-        <button onClick={onSignOut} className="profile__logout">Выйти из аккаунта</button>
+        <button
+          type="submit"
+          className="profile-form__submit"
+        >Редактировать</button>
+        <button
+          onClick={onSignOut}
+          className="profile__logout"
+        >Выйти из аккаунта</button>
       </form>
     </section>
   );
