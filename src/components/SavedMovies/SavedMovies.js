018a2d2a45b01b61565b69243c4b46dcf08c7d75
Stas Voronin <jameslebron@yandex.ru>
2021-09-19 21:20:30 +0300

    feat: добавил поиск по сохраненным фильмам

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
+import filterMovies from '../../utils/filterMovies';
 import './SavedMovies.css';
 
 function SavedMovies({ savedMoviesByCurrentUser, onMovieDelete }) {
+  const [filteredMovies, setFilteredMovies] = React.useState([]);
+
+  function handleSearch(query, checkboxStatus) {
+    const searchResult = filterMovies(savedMoviesByCurrentUser, query, checkboxStatus);
+    setFilteredMovies(searchResult);
+  }
 
   return (
   <section className="saved-movies">
-    <SearchForm />
+    <SearchForm
+      onSearch={handleSearch}
+    />
     <MoviesCardList
-      movies={savedMoviesByCurrentUser}
+      movies={filteredMovies.length > 0 ?
+        filteredMovies : savedMoviesByCurrentUser}
       isMoreButtonVisible={false}
       onMovieDelete={onMovieDelete}
     />
